5cce11ac84550152a90fad7f9aa2f7545f37612f
= <=>
2023-05-23 22:09:03 -0300

    Criação do projeto

---
+++ src/main/java/com/dev/backend/LojaVirtualApplication.java
@@ -0,0 +1,14 @@
+package com.dev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+}
