42ab683942ff26ed2a631e942d64ae2fa098ecce
Allan Fernando <allan-sjp@hotmail.com>
2023-06-06 19:11:27 -0300

    Criação de entidades

---
+++ src/main/java/com/dev/backend/entity/Endereco.java
@@ -0,0 +1,38 @@
+package com.dev.backend.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Entity
+@Table(name ="endereco")
+@Data
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+    private String cep;
+    private Long numero;
+    private String rua;
+    @ManyToOne
+    @JoinColumn(name = "idPessoa")
+    private Pessoa pessoa;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+    
+}
---
+++ src/main/java/com/dev/backend/entity/Produto.java
@@ -0,0 +1,44 @@
+package com.dev.backend.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Entity
+@Table(name ="produto")
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    private String descricaoCurta;
+    private String descricaoLonga;
+    private double valorCusto;
+    private double valorVenda;
+
+    @ManyToOne
+    @JoinColumn(name = "idCategoria")
+    private Categoria categoria;
+    @ManyToOne
+    @JoinColumn(name = "idMarca")
+    private Marca marca;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+    
+}
